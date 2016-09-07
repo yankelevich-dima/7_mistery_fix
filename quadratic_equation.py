16da3a93fb0827079d51f1270ca5b9a4b29c0fe4
Dmitry <yankelevich.dima@gmail.com>
2016-09-07 22:21:32 +0300

    script fixed

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,10 +2,19 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
+
+    if a == 0:
+        if b == 0:
+            return None, None
+        return -1 * c / b, None
+
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return root1, None
+    if discriminant >= 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        if discriminant == 0:
+            return root1, None
+        else:
+            return root1, root2
     else:
-        return root1, root2
+        return None, None
